4e2c2937c2335bdaba062184be8b7173797634b3
Delicia Brummitt <delicia@Delicias-MacBook-Pro.local>
2016-08-14 09:54:58 -0400

    Redact Twitter account info

--- config/index.js
+++ config/index.js
@@ -1,12 +1,12 @@
 const config = {};
 
 config.twitter = {};
-config.twitter.consumer_key = 'O7alZCqGXrAdR8tZK0IZePu4Q';
+config.twitter.consumer_key = 'ENTER KEY HERE';
 config.twitter.consumer_secret =
-  'YlMrUD6vb2S98U9JgaoU2Qy9IIaX2z8JSSBCHUv39WyFsUNbLw';
+  'ENTER SECRET HERE';
 config.twitter.access_token_key =
-  '348560436-MEZSu3G6yYaUUWyGZMBxJ31js4m44CrmEDxae8cM';
+  'ENTER TOKEN KEY HERE';
 config.twitter.access_token_secret =
-  'P12Xfk7ajb2kKAKlXMtq982BAPo8U3SiOcBQ6tmIPr6sU';
+  'ENTER TOKEN SECRET HERE';
 
 module.exports = config;
